63edecfe69611ab08615726434be06be85ab3d71
Felix Cosmin Mocanu <melccel@gmail.com>
2014-10-10 14:27:32 +0000

    hello, world!

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/***************************
+
+Hello world program in C++
+
+***************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout << "hello world!\n";
+    
+    return 0;
+}
